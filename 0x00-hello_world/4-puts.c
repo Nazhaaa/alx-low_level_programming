ca6d1976a2cdfce5a65d262cad0b9e8bfdb08152
Nazhaaa <nazhakhatib3@gmail.com>
2023-04-20 15:37:43 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  *
  *   *Return: Always 0 (Success)
  *
- *    */
+ *    
  * Return: Always 0 (Success)
  */
 
@@ -17,7 +17,6 @@ int main(void)
 	 return (0);
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
 }
 
 	
