5d315418d4d7f26a74fea8d4d7254f61b92da900
Nazhaaa <nazhakhatib3@gmail.com>
2023-04-25 11:40:47 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,21 +8,21 @@
  * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
-{	if (n > 0)
-	{
-		_putchar(43);
-		return (1);
-	} 
-	else if (n < 0)
-        {
-                _putchar(45);
-                   return (-1);
-		 }
-	      	else
-		{
-		_putchar(48);
-		return (0);
-	}
-		}
+{if (n > 0)
+{
+_putchar(43);
+return (1);
+} 
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
+}
+}
 
 
