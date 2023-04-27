d85285964969c5b953d486f7b5448552a5fdad54
Nazhaaa <nazhakhatib3@gmail.com>
2023-04-27 09:50:00 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,19 +6,19 @@
 
 void print_line(int n)
 {
-int i;
+	int i;
 
-if (n <= 0)
-{
-_putchar('\n');
-}
-else
-{
-for (i = 1; i <= n; i++)
-{
-_putchar('-');
-}
-_putchar('\n');
-}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('-');
+		}
+		_putchar('\n');
+	}
 
 }
